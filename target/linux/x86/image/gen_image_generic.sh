e5dfc482cc72f8993b70f77f6e2791c434e4854d
Adrien Gallouët <adrien@gallouet.fr>
2017-07-26 21:49:14 +0200

    x86: Add UEFI
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- target/linux/x86/image/gen_image_generic.sh
+++ target/linux/x86/image/gen_image_generic.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 # Copyright (C) 2006-2012 OpenWrt.org
+
 set -x
-[ $# == 5 -o $# == 6 ] || {
+
+[ $# == 5 ] || [ $# == 6 ] || {
     echo "SYNTAX: $0 <file> <kernel size> <kernel directory> <rootfs size> <rootfs image> [<align>]"
     exit 1
 }
@@ -15,23 +17,22 @@ ALIGN="$6"
 
 rm -f "$OUTPUT"
 
-head=16
-sect=63
-cyl=$(( ($KERNELSIZE + $ROOTFSSIZE) * 1024 * 1024 / ($head * $sect * 512)))
-
-# create partition table
-set `ptgen -o "$OUTPUT" -h $head -s $sect -p ${KERNELSIZE}m -p ${ROOTFSSIZE}m ${ALIGN:+-l $ALIGN} ${SIGNATURE:+-S 0x$SIGNATURE}`
+# shellcheck disable=2046
+set $(ptgen -o "$OUTPUT" -h 16 -s 63 -t 0xef -p "${KERNELSIZE}m" -t 0x83 -p "${ROOTFSSIZE}m" ${ALIGN:+-l "$ALIGN"} ${SIGNATURE:+-S "0x$SIGNATURE"})
 
 KERNELOFFSET="$(($1 / 512))"
-KERNELSIZE="$2"
+KERNELCOUNT="$(($2 / 1024))" # mkfs.fat BLOCK_SIZE=1024
 ROOTFSOFFSET="$(($3 / 512))"
-ROOTFSSIZE="$(($4 / 512))"
+ROOTFSCOUNT="$(($4 / 512))"
 
-[ -n "$PADDING" ] && dd if=/dev/zero of="$OUTPUT" bs=512 seek="$ROOTFSOFFSET" conv=notrunc count="$ROOTFSSIZE"
+[ -n "$PADDING" ] && \
+	dd if=/dev/zero of="$OUTPUT" bs=512 seek="$ROOTFSOFFSET" count="$ROOTFSCOUNT"
+
 dd if="$ROOTFSIMAGE" of="$OUTPUT" bs=512 seek="$ROOTFSOFFSET" conv=notrunc
 
 [ -n "$NOGRUB" ] && exit 0
 
-make_ext4fs -J -l "$KERNELSIZE" "$OUTPUT.kernel" "$KERNELDIR"
+mkfs.fat -C -n BOOT "$OUTPUT.kernel" "$KERNELCOUNT"
+mcopy -s -i "$OUTPUT.kernel" "$KERNELDIR"/* ::/
 dd if="$OUTPUT.kernel" of="$OUTPUT" bs=512 seek="$KERNELOFFSET" conv=notrunc
 rm -f "$OUTPUT.kernel"
